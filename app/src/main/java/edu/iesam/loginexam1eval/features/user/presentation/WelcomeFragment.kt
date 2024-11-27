15412320123ab61bbb075b4afe4c250016c850a2
Alessandro Paucar <apaucare27@gmail.com>
2024-11-27 04:47:09 +0100

    feature: go back button

--- app/src/main/java/edu/iesam/loginexam1eval/features/user/presentation/WelcomeFragment.kt
+++ app/src/main/java/edu/iesam/loginexam1eval/features/user/presentation/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import edu.iesam.loginexam1eval.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -18,5 +19,12 @@ class WelcomeFragment : Fragment() {
     ): View? {
         _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.action.setOnClickListener{
+            findNavController().navigate(WelcomeFragmentDirections.welcomeToSignUp())
+        }
     }
 }
